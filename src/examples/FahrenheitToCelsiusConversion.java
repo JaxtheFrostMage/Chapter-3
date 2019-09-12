20727b580e64d8eb2a9062f3af45ac59ed0caa05
Evan Raker <er306632@students.davenport.k12.ia.us>
2019-09-12 08:32:39 -0500

    conversions

--- src/examples/FahrenheitToCelsiusConversion.java
+++ src/examples/FahrenheitToCelsiusConversion.java
@@ -6,24 +6,54 @@ public class FahrenheitToCelsiusConversion {
 
     public static void main(String[] args){
 
-        int tempAt8am;
-        int tempAt12am;
-        int tempAt5pm;
+        double tempAt8am;
+        double tempAt12am;
+        double tempAt5pm;
+        int pick = 0;
 
         System.out.println("What was the temperature at 8:00 AM?");
         Scanner inputScanner = new Scanner(System.in);
-        tempAt8am = inputScanner.nextInt();
+        tempAt8am = inputScanner.nextDouble();
         System.out.println("What was the temperature at 12:00 AM?");
         inputScanner = new Scanner(System.in);
-        tempAt12am = inputScanner.nextInt();
+        tempAt12am = inputScanner.nextDouble();
         System.out.println("What was the temperature at 5:00 PM?");
         inputScanner = new Scanner(System.in);
-        tempAt5pm = inputScanner.nextInt();
+        tempAt5pm = inputScanner.nextDouble();
+
+        fahrenheitToCelsius(tempAt8am, tempAt12am, tempAt5pm);
+    }
+
+    public static void fahrenheitToCelsius(double tempAt8am, double tempAt12am, double tempAt5pm){
+
+        double conversion8;
+        double conversion12;
+        double conversion5;
+
+        conversion8 = (tempAt8am - 32) * 5 / 9;
+        conversion12 = (tempAt12am - 32) * 5 / 9;
+        conversion5 = (tempAt5pm - 32) * 5 / 9;
+
+        System.out.println(tempAt8am + " Fahrenheit is equivalent to " + conversion8 + " Celsius");
+        if(tempAt8am < 32){
+            System.out.println("That's Cold!");
+        }else if(tempAt8am > 100){
+            System.out.println("That's Hot!");
+        }
+        System.out.println(tempAt12am + " Fahrenheit is equivalent to " + conversion12 + " Celsius");
+        if(tempAt12am < 32){
+            System.out.println("That's Cold!");
+        }else if(tempAt12am > 100){
+            System.out.println("That's Hot!");
+        }
+        System.out.println(tempAt5pm + " Fahrenheit is equivalent to " + conversion5 + " Celsius");
+        if(tempAt5pm < 32){
+            System.out.println("That's Cold!");
+        }else if(tempAt5pm > 100){
+            System.out.println("That's Hot!");
+        }
 
 
     }
-    public static void temperatureConversion(int tempAt8am, int tempAt12am, int tempAt5pm){
 
-
-    }
 }
